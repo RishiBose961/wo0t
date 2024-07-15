1abfe4729c0b38a98d545a31eefb2e1a3c73b961
Rishi <rishibose1901@gmail.com>
2024-07-15 09:34:44 +0530

    update to socket io issue

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://wo0t.vercel.app/"],
     methods: ["GET", "POST"],
   },
 });
